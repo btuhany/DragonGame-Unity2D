f50f0c7961b45c1bd4fe072fa562714d0ea9133c
btuhany <114235184+btuhany@users.noreply.github.com>
2022-12-26 19:35:59 +0300

    GameOverPanel added

--- DragonAttack/Assets/GameFolders/Scripts/UIs/DisplayScore.cs
+++ DragonAttack/Assets/GameFolders/Scripts/UIs/DisplayScore.cs
@@ -17,12 +17,13 @@ namespace UIs
         {
             GameManager.Instance.OnScoreChanged += HandleOnScoreChanged;  //we add the function that we should run to
                                                                           //event thus, game manager can run this function.
+            HandleOnScoreChanged();          //to view the score as 0 initally
         }
         private void OnDisable()
         {
             GameManager.Instance.OnScoreChanged -= HandleOnScoreChanged; //
         }
-        private void HandleOnScoreChanged(int score)
+        private void HandleOnScoreChanged(int score = 0)
         {
             _scoreText.text = "Score: " + score;
         }
